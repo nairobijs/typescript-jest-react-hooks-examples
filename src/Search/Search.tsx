2d59e47c773341f33e737037f7f968c295db420c
Sam Kasyoki <samkasyoki@gmail.com>
2020-11-13 08:23:20 +0300

    add correct type for input onChange event

--- src/Search/Search.tsx
+++ src/Search/Search.tsx
@@ -1,50 +1,44 @@
-import React, { EventHandler } from 'react';
- 
+import React from 'react';
+
 function App() {
   const [search, setSearch] = React.useState('');
- 
-  function handleChange(event:any) {
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value);
   }
- 
+
   return (
     <div>
       <Search value={search} onChange={handleChange}>
         Search:
       </Search>
- 
+
       <p>Searches for {search ? search : '...'}</p>
     </div>
   );
 }
 
-
-
 interface User {
   firstName: string;
   lastName: string;
 }
+
 interface Props {
   value: string;
-  onChange: (event: any) => void;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   children: string;
   headers?: {};
-  user?:User;
+  user?: User;
   other?: boolean;
 }
 
-const Search:React.FC<Props> = function ({ value, onChange, children}) {
+const Search: React.FC<Props> = function ({ value, onChange, children }) {
   return (
     <div>
       <label htmlFor="search">{children}</label>
-      <input
-        id="search"
-        type="text"
-        value={value}
-        onChange={onChange}
-      />
+      <input id="search" type="text" value={value} onChange={onChange} />
     </div>
   );
-}
- 
+};
+
 export default App;
